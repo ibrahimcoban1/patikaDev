1076d0929975d89f23567a35d6471f04764af5a8
ibrahim coban <ibosss9192@gmail.com>
2022-07-25 15:37:49 +0300

    kod eklendi

---
+++ taksimetre/taksimetre.java
@@ -0,0 +1,22 @@
+package patikaDev.taksimetre;
+
+import java.util.Scanner;
+
+public class taksimetre {
+    public static void main(String[] args) {
+        int km;
+        double perKm = 2.20,total,startPrice =10;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Mesafeyi KM cinsinden giriniz: ");
+
+        km = input.nextInt();
+
+        total = perKm*km;
+        total += startPrice;
+
+        total = (total<20) ? 20:total;
+        System.out.println(total);
+    }
+}
